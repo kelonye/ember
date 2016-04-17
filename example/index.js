abd1e0cfc7770dc6d9821dfb35de1ae686379fa1
Mitchel Kelonye <kelonyemitchel@gmail.com>
2016-04-17 11:22:40 +0300

    Release 2.5.0

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@ require('ember');
 
 App = Em.Application.create();
 
-Em.TEMPLATES.index = Em.Handlebars.template(eval(require('./template')));
+Em.TEMPLATES.index = Em.Handlebars.template(require('./template'));
 
 App.IndexRoute = Em.Route.extend({
   model: function(){
